f47af58e7770b9d2fcde313cd013e8d75d2cae8e
andresh <shahiandy123@gmail.com>
2024-11-19 22:30:04 +0545

    first

--- app/src/main/java/com/example/firstapp/MainActivity2.kt
+++ app/src/main/java/com/example/firstapp/MainActivity2.kt
@@ -1,38 +1,16 @@
 package com.example.firstapp
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginForm : AppCompatActivity() {
-    lateinit var First: First
-    lateinit var Last: Last
-    lateinit var Email: Email
-    lateinit var Password: Password
-    lateinit var button: Button
-
-
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_form)
-
-
-
-        First=findViewById(R.id.First)
-        Last= findViewById(R.id.Last)
-        Email= findViewById(R.id.Email)
-        Password=findViewById(R.id.Password)
-
-        button.setOnClickListener{
-            val data:String=Password.text.toString()
-            Password.text =data
-
+        setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
